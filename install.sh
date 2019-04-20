528e9b08ab99ab8550488a24dd70a9c6ae4b92b4
qiruisun_mac <sqr@detu.com>
2019-04-21 00:30:28 +0800

    跨平台

---
+++ install.sh
@@ -0,0 +1,8 @@
+export GO111MODULE=off
+go get -u golang.org/x/mobile/cmd/gobind
+go get -u golang.org/x/mobile/cmd/gomobile
+
+export GO111MODULE=off
+go get -u github.com/go-gl/gl
+go get -u github.com/go-gl/glfw 
+go get -u golang.org/x/mobile
--- run.sh
+++ run.sh
@@ -1,6 +1,57 @@
-export GO111MODULE=off
-go get -u github.com/suiqirui1987/fly3d/tools/logrus
-go get -u github.com/go-gl/gl
-go get -u github.com/go-gl/glfw 
-go get -u golang.org/x/mobile
-gopherjs  build
+#! /bin/sh
+
+
+js(){
+     echo "js"
+
+    gopherjs  build
+}
+
+android(){
+    echo "android"
+
+    gomobile build -target=android -bundleid demo
+}
+
+ios(){
+    echo "IOS" 
+
+    gomobile build -target=ios -bundleid demo
+}
+
+os(){
+    OS=`uname -s`
+    echo "config go env"
+    export GOPATH=$GOPATH:$currPWD
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.io
+
+
+    echo "output demo os "
+    gox -os="linux" -arch="amd64" -output="./bin/demo_linux64" ./
+    gox -os="darwin" -arch="amd64" -output="./bin/demo_darwin64" ./
+    gox -os="windows" -arch="amd64" -output="./bin/demo_windows" ./
+
+
+
+}
+
+case "$1" in
+  js)
+	js
+        ;;
+  android)
+	android
+        ;;
+  ios)
+	ios
+        ;;
+  os)
+	os
+        ;;
+  *)
+	FULLPATH=/etc/init.d/$PIDNAME
+	echo "Usage: $FULLPATH {start|stop|restart|force-reload|status|configtest|terminate}"
+	exit 1
+	;;
+esac
